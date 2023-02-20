ae86d1a7911dc636f56f7ad5d72505357ada30a5
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2023-02-20 12:38:49 +0530

    make components for login, register

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
 import React from 'react';
 
 import './App.css';
+import { Login } from './components/Login';
+import { Register } from './components/Register';
 
 function App() {
-  return (
-    <main className="form-signin">
-      <form>
-
-        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-
-        <div className="form-floating">
-          <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-          <label htmlFor="floatingInput">Email address</label>
-        </div>
-        <div className="form-floating">
-          <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-          <label htmlFor="floatingPassword">Password</label>
-        </div>
-
-        
-        <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        
-      </form>
-    </main>
-  );
+  return <Register/>;
 }
 
 export default App;
---
+++ src/components/Register.tsx
@@ -0,0 +1,36 @@
+
+export const Register=()=>{
+    return <main className="form-signin">
+        <form>
+
+            <h1 className="h3 mb-3 fw-normal">Please register</h1>
+
+            <div className="form-floating">
+                <input type="text" className="form-control" id="floatingInput" placeholder="First name" />
+                <label htmlFor="floatingInput">First Name</label>
+            </div>
+
+            <div className="form-floating">
+                <input type="text" className="form-control" id="floatingInput" placeholder="Last name" />
+                <label htmlFor="floatingInput">Last Name</label>
+            </div>
+
+            <div className="form-floating">
+                <input type="email" className="form-control" id="floatingInput" placeholder="email" />
+                <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <div className="form-floating">
+                <input type="password" className="form-control" id="floatingPassword" placeholder="Password Confirm" />
+                <label htmlFor="floatingPassword">Password Confirm</label>
+            </div>
+
+
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+
+        </form>
+    </main>
+}
